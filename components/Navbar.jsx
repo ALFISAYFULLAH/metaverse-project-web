8a800aa4090afbcc0d4582aadf1bfda397816e32
Alfi WebGen ID <wgenesis.pro@gmail.com>
2023-12-11 05:11:23 +0700

    navbar hero about section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,18 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
 
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+    <motion.nav variants={navVariants} initial="hidden" whileInView="show" className={`${styles.xPaddings} py-8 relative`}>
+        <div className="absolute w-[50%] inset-0 gradient-01"></div>
+        <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+            <img className="w-6 h-6 object-contain" src="/search.svg" alt="search" />
+            <h1 className="font-extrabold text-2xl leading-8 text-white uppercase">metaversus</h1>
+            <img className="w-6 h-6 object-contain" src="/menu.svg" alt="menu" />
+        </div>
+    </motion.nav>
 );
 
 export default Navbar;
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,9 +1,36 @@
-'use client';
+"use client";
 
-const Hero = () => (
-  <section>
-    Hero section
-  </section>
-);
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion"
 
+const Hero = () => {
+    return (
+        <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+            <motion.div variants={staggerContainer} initial="hidden" whileInView="show" viewport={{ once: false, amount: 0.25 }} className={`${styles.innerWidth} flex flex-col mx-auto`}>
+                <div className="flex justify-center items-center flex-col relative z-10">
+                    <motion.h1 variants={textVariant(1, 1)} className={styles.heroHeading}>
+                        Metaverse
+                    </motion.h1>
+                    <motion.div variants={textVariant(1.2)} className="flex items-center">
+                        <h1 className={styles.heroHeading}>Ma</h1>
+                        <div className={styles.heroDText}></div>
+                        <h1 className={styles.heroHeading}>ness</h1>
+                    </motion.div>
+                </div>
+
+                <motion.div variants={slideIn("right", "tween", 0.2, 1)} className="relative w-full md:-mt-5 -mt-3">
+                    <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top-8"/>
+                        <img className="w-full h-[300px] sm:h-[500px] object-cover rounded-tl-[140px] z-10 relative" src="/cover.png" alt="" />
+
+                        <a href="#explore">
+                            <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+                                <img className="w-24 sm:w-40 h-24 sm:h-40 object-cover rounded-full border-4 border-[#6a6a6a]" src="/stamp.png" alt="stamp" />
+                            </div>
+                        </a>
+                </motion.div>
+            </motion.div>
+        </section>
+    );
+}
 export default Hero;
